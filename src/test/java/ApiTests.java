1227f709f1cf3afef9899648952aa504305477d7
rahul-uj <rahuluj@testvagrant.com>
2022-12-30 15:58:15 +0530

    Further API TEST SCRIPTS

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,5 +12,26 @@ public class ApiTests {
                 .then()
                     .statusCode(200)
                     .log().body();
+    }
+
+    @Test
+    public void saveUser(){
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization","Bearer f71a00fb0610866efbd24ef6b398a488e1ec69958a34886ad59a9710f7d0729d")
+                .body("{\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"Jayanthi\",\n" +
+                        "    \"email\": \"jayanthi@gmail.com\",\n" +
+                        "    \"gender\": \"female\",\n" +
+                        "    \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                .post("https://gorest.co.in/public/v2/users")
+                .then()
+                .log()
+                .body()
+                .statusCode(201);
     }
 }
